fffc6236b1c8be6f67b1939509fdd9b5eec9d9ab
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-13 13:52:53 +0200

    Create index.ts

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+// PATH: sanity/schemas/index.ts
+import category from './category'
+import region from './region'
+import translation from './translation'
+
+export const schemaTypes = [category, region, translation]
