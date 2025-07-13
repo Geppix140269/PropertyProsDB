ec62de398ea9891c958a69ce8fd23aba35f80418
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-13 14:00:55 +0200

    Create page.tsx

---
+++ app/studio/[[...index]]/page.tsx
@@ -0,0 +1,9 @@
+// PATH: app/studio/[[...index]]/page.tsx
+'use client'
+
+import { NextStudio } from 'next-sanity/studio'
+import config from '@/sanity.config'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
