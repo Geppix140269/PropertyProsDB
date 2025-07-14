5fe4fc5c996536caaef18d70c9c60b81cbee9dc8
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-14 22:18:03 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-// app/page.tsx
-import Hero from './components/home/Hero'
-import UserTypeCards from './components/home/UserTypeCards'
-import HowItWorks from './components/home/HowItWorks'
-import SurveyPackages from '@/components/home/SurveyPackages';
-import SecurityFeatures from './components/home/SecurityFeatures'
-import CTASection from './components/home/CTASection'
+// PATH: app/page.tsx
+import Hero from '@/components/home/Hero'
+import UserTypeCards from '@/components/home/UserTypeCards'
+import HowItWorks from '@/components/home/HowItWorks'
+import SurveyPackages from '@/components/home/SurveyPackages'
+import WhyUseApulink from '@/components/home/WhyUseApulink'
+import CTASection from '@/components/home/CTASection'
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <UserTypeCards />
       <HowItWorks />
       <SurveyPackages />
-      <SecurityFeatures />
+      <WhyUseApulink />
+      <UserTypeCards />
       <CTASection />
     </>
   )
