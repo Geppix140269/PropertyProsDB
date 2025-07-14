50044400ac2ae4de2f4f71c2c4e7307b81e678df
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-14 21:00:14 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 // app/page.tsx
-import Hero from '@/components/home/Hero'
-import UserTypeCards from '@/components/home/UserTypeCards'
-import HowItWorks from '@/components/home/HowItWorks'
-import SecurityFeatures from '@/components/home/SecurityFeatures'
-import CTASection from '@/components/home/CTASection'
+import Hero from './components/home/Hero'
+import UserTypeCards from './components/home/UserTypeCards'
+import HowItWorks from './components/home/HowItWorks'
+import SecurityFeatures from './components/home/SecurityFeatures'
+import CTASection from './components/home/CTASection'
 
 export default function HomePage() {
   return (
