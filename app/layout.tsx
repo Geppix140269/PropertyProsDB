9bd47ca13e4bd8180f2d322ba90329c333946fc0
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-16 19:08:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import TrulloChatbot from '@/components/TrulloChatbot'
 import type { Metadata } from "next";
 import { Playfair_Display, Open_Sans } from "next/font/google";
-import "./global.css";
+import "./globals.css";
 import { LanguageProvider } from './providers/language-provider';
 
 const playfair = Playfair_Display({ 
